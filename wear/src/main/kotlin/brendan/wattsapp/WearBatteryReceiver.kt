edd27e0883668c3e86d73ec9f4c199d60d279c25
Brendan Fahy <brendan.fahy@gmail.com>
2017-01-26 22:28:09 +0000

    Adjust for formatting.

--- wear/src/main/kotlin/brendan/wattsapp/WearBatteryReceiver.kt
+++ wear/src/main/kotlin/brendan/wattsapp/WearBatteryReceiver.kt
@@ -13,9 +13,17 @@ class WearBatteryReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         val displayIntent = Intent(context, WearDisplayActivity::class.java)
         val text = intent.getStringExtra(CONTENT_KEY)
-        val notification = Notification.Builder(context).setSmallIcon(R.mipmap.ic_launcher).setContentTitle(text).extend(Notification.WearableExtender().setDisplayIntent(PendingIntent.getActivity(context, 0, displayIntent,
-                PendingIntent.FLAG_UPDATE_CURRENT))).build()
-        (context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager).notify(0, notification)
+        val notification = Notification.Builder(context)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(text)
+                .extend(Notification.WearableExtender().setDisplayIntent(
+                        PendingIntent.getActivity(context, 0, displayIntent,
+                                PendingIntent.FLAG_UPDATE_CURRENT))
+                )
+                .build()
+
+        (context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager)
+                .notify(0, notification)
 
         Toast.makeText(context, context.getString(R.string.notification_posted), Toast.LENGTH_SHORT).show()
     }
